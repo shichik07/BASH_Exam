af229695ed9026373ad52382ca5ada6560c10e24
Julius Kricheldorff <julius@kricheldorff.de>
2025-03-02 12:59:17 +0000

    modified exam.sh file successfully

--- exam_KRICHELDORFF/exam_BASH/exam.sh
+++ exam_KRICHELDORFF/exam_BASH/exam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cards=('rtx3060' 'rtx3070' 'rtx3080' 'rtx3090' 'rx6700')
-for x in 'rtx3060' 'rtx3070' 'rtx3080' 'rtx3090' 'rx6700'
+echo "$(date)" >> sales.txt
+for x in "${cards[@]}"
 do
-tmp = (curl "http://0.0.0.0:5000/$x")
-echo "$x $tmp" >> sales.txt
+tmp=$(curl "http://0.0.0.0:5000/$x")
+echo "$x":" $tmp" >> sales.txt
 done 
