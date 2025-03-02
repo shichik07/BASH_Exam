2b577a60e22cc071b69a15e59f13ec319f6faac4
Ubuntu <ubuntu@ip-172-31-46-46.eu-west-1.compute.internal>
2025-03-02 15:50:17 +0000

    included function now

--- exam_KRICHELDORFF/exam_BASH/exam.sh
+++ exam_KRICHELDORFF/exam_BASH/exam.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cards=('rtx3060' 'rtx3070' 'rtx3080' 'rtx3090' 'rx6700')
-echo "$(date)" >> sales.txt
+
+get_data(){
+    echo $(curl "http://0.0.0.0:5000/$1")
+}
+
+echo "$(date)" >> sales2.txt
 for x in "${cards[@]}"
 do
-tmp=$(curl "http://0.0.0.0:5000/$x")
-echo "$x":" $tmp" >> sales.txt
+tmp=$(get_data "$x")
+echo "$x":" $tmp" >> sales2.txt
 done 
